e377421f2632806ca7add4dcd238ac0be4f36c83
ericgold <egold05@gmail.com>
2017-08-16 19:56:56 -0700

    separate Navigation

--- src/modules/App/components/Navigation.js
+++ src/modules/App/components/Navigation.js
@@ -7,7 +7,15 @@ import Nav from 'react-bootstrap/lib/Nav';
 import NavItem from 'react-bootstrap/lib/NavItem';
 
 export default class Navigation extends Component {
-	  static contextTypes = {
+    static propTypes = {
+      children: PropTypes.object.isRequired,
+      user: PropTypes.object,
+      notifs: PropTypes.object,
+      logout: PropTypes.func.isRequired,
+      pushState: PropTypes.func.isRequired
+  };
+
+  static contextTypes = {
     store: PropTypes.object.isRequired
   };
 
@@ -34,51 +42,54 @@ export default class Navigation extends Component {
       this.props.push('/login');
     });
   };
+
   render() {
-  	const { user, notifs, children } = this.props;
-  	const styles = require('../App.scss');
+    const { user, notifs, children } = this.props;
 
-  	return (
-  		<Navbar fixedTop>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <IndexLink to="/">
-              {/* <IndexLink to="/" activeStyle={{ color: '#33e0ff' }}> */}
-                <div className={styles.brand} />
-              </IndexLink>
-            </Navbar.Brand>
+    const styles = require('../App.scss');
+    return (
+      <Navbar fixedTop>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <IndexLink to="/">
+            {/* <IndexLink to="/" activeStyle={{ color: '#33e0ff' }}> */}
+              <div className={styles.brand} />
+            </IndexLink>
+          </Navbar.Brand>
 
-            { user ? <Navbar.Toggle /> : null }
-          </Navbar.Header>
+          { user ? <Navbar.Toggle /> : null }
+        </Navbar.Header>
 
-          { user ?
-            <Navbar.Collapse>
-              <Nav navbar pullRight>
-                <LinkContainer to="/profile">
-                  <NavItem eventKey={0}>Profile</NavItem>
-                </LinkContainer>
-                {/* {!user && <LinkContainer to="/login">
-                  <NavItem eventKey={5}>Login</NavItem>
-                </LinkContainer>} */}
-                {/* {!user && <LinkContainer to="/register">
-                  <NavItem eventKey={6}>Register</NavItem>
-                </LinkContainer>} */}
-                <LinkContainer to="/logout">
-                  <NavItem eventKey={1} className="logout-link" onClick={this.handleLogout}>
-                    Logout
-                  </NavItem>
-                </LinkContainer>
-              </Nav>
-              <Nav navbar pullRight>
-                {user && <span className="navbar-text">
-                  Hello <strong>{user.first}!</strong>
-                </span>}
-              </Nav>
-            </Navbar.Collapse>
-          : null }
-        </Navbar>
-  	);
+        { user ?
+          <Navbar.Collapse>
+            <Nav navbar pullRight>
+              <LinkContainer to="/profile">
+                <NavItem eventKey={0}>Profile</NavItem>
+              </LinkContainer>
+              {/* {!user && <LinkContainer to="/login">
+                <NavItem eventKey={5}>Login</NavItem>
+              </LinkContainer>} */}
+              {/* {!user && <LinkContainer to="/register">
+                <NavItem eventKey={6}>Register</NavItem>
+              </LinkContainer>} */}
+              <LinkContainer to="/logout">
+                <NavItem eventKey={1} className="logout-link" onClick={this.handleLogout}>
+                  Logout
+                </NavItem>
+              </LinkContainer>
+            </Nav>
+            <Nav navbar pullRight>
+              {user && <span className="navbar-text">
+                Hello <strong>{user.first}!</strong>
+              </span>}
+            </Nav>
+          </Navbar.Collapse>
+        : null }
+      </Navbar>
+    )
   }
 }
-	
+
+
+
 
