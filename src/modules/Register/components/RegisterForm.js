498ba96a2163ba6180448dca8d7b2adb4859522d
ericgold <egold05@gmail.com>
2017-08-20 17:30:04 -0700

    set label and input wrapper classes to full width of container as in wireframe

--- src/modules/Register/components/RegisterForm.js
+++ src/modules/Register/components/RegisterForm.js
@@ -21,8 +21,8 @@ export default class RegisterForm extends Component {
 
   renderInput = ({ input, label, type, meta: { touched, error } }) =>
     <div className={`form-group ${error && touched ? 'has-error' : ''}`}>
-      <label htmlFor={input.name} className="col-sm-2">{label}</label>
-      <div className="col-sm-10">
+      <label htmlFor={input.name} className="col-sm-12">{label}</label>
+      <div className="col-sm-12">
         <input {...input} type={type} className="form-control" />
         {error && touched && <span className="glyphicon glyphicon-remove form-control-feedback"></span>}
         {error && touched && <div className="text-danger"><strong>{error}</strong></div>}
