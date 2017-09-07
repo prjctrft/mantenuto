0709efa50fe56c2402b02347d39e26cae760d213
Brandon Benefield <bsquared18@gmail.com>
2017-09-07 16:18:47 -0400

    Component tests (#18)
    * Push
    
    * .
    
    * test(<Home />): add test for home page
    
    * test(<Home />): add missing test to home
    
    * Delete SpeakeasyForm.test.js
    
    No longer working or a valid test
    
    * Delete Home.test.js
    
    No longer a working or valid test
    
    * finalize PR

---
+++ src/modules/Home/Home.test.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { expect } from 'chai';
+import { mount } from 'enzyme';
+import Home from './Home';
+import { Provider } from 'react-redux';
+import { browserHistory } from 'react-router';
+
+import createStore from 'redux/create';
+import ApiClient from 'helpers/ApiClient';
+const client = new ApiClient();
+
+describe('<Home />', () => {
+
+  const mockStore = {
+  };
+
+  const store = createStore(browserHistory, client, mockStore);
+  const connectedComponent = mount(
+    <Provider store={store} key="provider">
+      <Home />
+    </Provider>
+  );
+
+  const component = connectedComponent.find(Home);
+
+  it('should render correctly', () => {expect(component.exists()).to.be.true});
+
+  it('should render div with class home', () => {
+    const home = component.find('.home');
+    expect(home).to.have.length(1);
+    expect(home.name()).to.equal('div');
+  });
+
+  it('should render image', () => {
+    expect(component.find('img').exists()).to.be.true;
+  });
+
+  it('should render proper text under image', () => {
+    expect(component.find('a').text()).to.equal('Already a member?');
+  });
+
+  it('should render h1 element', () => {
+    expect(component.find('h1')).to.have.length(1);
+  });
+});
