0c8eaea9c6ddd55b08e7d6e889eb7cdc2505168f
Brandon Benefield <bsquared18@gmail.com>
2017-09-14 19:28:40 -0400

    feat(password): add UI to Forgot.js

--- src/modules/Password/components/Forgot.js
+++ src/modules/Password/components/Forgot.js
@@ -1,5 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router';
+
+function Main(props) {	
+	const styles = require('../Password.scss');
+	const sendEmailButton = function () {
+		//do logic
+	};
+
+	return (
+    <div className={ styles.content }>
+      <h1>Forgot your password?</h1>
+      <p>No problem!</p>
+
+      <form action="/password/CheckEmail">
+	      <input className="form-control" name="emailInput" placeholder="E-mail" />
+	      <button type="submit" onClick={ sendEmailButton }>Send</button>
+      </form>
+    </div>
+	);
+}
 
 export default (props) => {
-  return (<h1>Password component!</h1>)
+  return (
+  	<Main />
+  );
 }
