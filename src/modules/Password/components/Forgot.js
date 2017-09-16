c5f0703ed6536dbe8b15fd5e285193aee7fcd1b8
Brandon Benefield <bsquared18@gmail.com>
2017-09-15 22:07:33 -0400

    refactor(Forgot.js): change className & change form action remove import { Link }

--- src/modules/Password/components/Forgot.js
+++ src/modules/Password/components/Forgot.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router';
 
-function Main(props) {	
+function Body(props) {	
 	const styles = require('../Password.scss');
 	const sendEmailButton = function () {
 		//check if email entered is a valid email and is within the database
@@ -10,9 +9,9 @@ function Main(props) {
 	return (
     <div className={ styles.content }>
       <h1>Forgot your password?</h1>
-      <p>No problem!</p>
+      <p className="h1">No problem!</p>
 
-      <form action="/password/CheckEmail">
+      <form action="./forgot">
 	      <input className="form-control" type="email" name="emailInput" placeholder="E-mail" />
 	      <button type="submit" onClick={ sendEmailButton }>Send</button>
       </form>
@@ -22,6 +21,6 @@ function Main(props) {
 
 export default (props) => {
   return (
-  	<Main />
+  	<Body />
   );
 }
