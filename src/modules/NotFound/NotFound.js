54803c259e64c9362efb9a67003eea0779660337
Brandon Benefield <bsquared18@gmail.com>
2017-09-24 17:05:28 -0400

    fix(NotFound): set style to center text in NotFound component (#41)

--- src/modules/NotFound/NotFound.js
+++ src/modules/NotFound/NotFound.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
 const NotFound = () => {
+  const styles = require('./NotFound.scss');
+
   return (
-    <div className="container">
+    <div className={styles.container}>
       <h1>Sorry!</h1>
       <p>We <em>can{"'"}t</em> find the page the you are looking for!</p>
     </div>
