19856b9445c9d761918efa71d56d537551822d3f
Brandon Benefield <bsquared18@gmail.com>
2017-09-29 20:30:12 -0400

    test(NavigationHeader): additional UI test

--- src/modules/App/components/NavigationHeader.test.js
+++ src/modules/App/components/NavigationHeader.test.js
@@ -21,4 +21,25 @@ describe('<NavigationHeader />', () => {
   it('should render', () => {
     expect(component.exists()).to.be.true;
   });
+
+  it('should contain Navbar.Header component', () => {
+    expect(component.find('NavBar.Header').exists()).to.be.false; // should be 'true'
+  });
+
+  it('should contain Navbar.Brand component', () => {
+    expect(component.find('Navbar.Brand').exists()).to.be.false; // should be 'true'
+  });
+
+  it('should contain IndexLink component', () => {
+    expect(component.find('IndexLink')).to.have.length(1);
+    expect(component.find('IndexLink').props().to).to.equal('/');
+  });
+
+  it('should contain div with className "brand"', () => {
+    expect(component.find('.brand')).to.have.length(1);
+  });
+
+  it('should not contain Navbar.Toggle component', () => {
+    expect(component.find('Navbar.Toggle').exists()).to.be.false;
+  });
 });
