7f0c9b1416c5d2422074f6f1fd8028a6663c466f
hdngr <hstnhdngr@gmail.com>
2017-10-10 15:36:30 -0700

    test(TryAuth): fix test based on updated props

--- src/modules/Auth/TryAuth.test.js
+++ src/modules/Auth/TryAuth.test.js
@@ -51,7 +51,7 @@ describe('<TryAuth />', () => {
   it('should run `socketAuth` on the client on "connect" event if already authed', () => {
     global.__SERVER__ = false;
     global.__CLIENT__ = true;
-    const newProps = { user: '1234', triedSocketAuth: false };
+    const newProps = { authenticated: true, triedSocketAuth: false };
     mount(
       <TryAuthComponent {...{...props, ...newProps}} />
     );
@@ -59,6 +59,7 @@ describe('<TryAuth />', () => {
     global.socket.emit('connect');
     expect(listeners).to.have.length(1);
     expect(listeners[0]).to.equal('connect');
+    debugger;
     expect(props.socketAuth.mock.calls).to.have.length(1);
   });
 
