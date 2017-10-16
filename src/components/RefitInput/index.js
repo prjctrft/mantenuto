a1367502cdb0a9476735cbb31314d5c56b458e2f
Huston Hedinger <hstnhdngr@gmail.com>
2017-10-16 12:38:17 -0700

    test(RefitInput): text RefitInput and lint (#66)

--- src/components/RefitInput/index.js
+++ src/components/RefitInput/index.js
@@ -1,25 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+// used in redux-form <Field /> Component
+// e.g. <Field ... component={RefitInput} />
 const RefitInput  = ({ size, labelRequired, input, label, type, meta: { touched, error, dirty, initial, pristine, valid } }) => { //eslint-disable-line no-unused-vars
 	// const styles = require('./index.scss');
 	const formGroupSize = size ? `form-group-${size}` : '';
 	const requiredLabel = labelRequired ? <span className="text-warning">*</span> : null;
 	return (
-    <div className={`form-group ${formGroupSize} ${touched ? 'has-feedback' : ''} ${valid && touched ? 'has-success' : '' } ${error && touched ? 'has-error' : ''}`}>
-      <label htmlFor={input.name}>{requiredLabel}{label}</label>
-      <input {...input} type={type} className='form-control' />
-			{valid && touched && <span className="glyphicon glyphicon-ok form-control-feedback" />}
-			{error && touched && <span className="glyphicon glyphicon-remove form-control-feedback" />}
-      {error && touched && <div className="text-danger"><strong>{error}</strong></div>}
-    </div>
+  <div className={`form-group ${formGroupSize} ${touched ? 'has-feedback' : ''} ${valid && touched ? 'has-success' : '' } ${error && touched ? 'has-error' : ''}`}>
+    <label htmlFor={input.name}>{requiredLabel}{label}</label>
+    <input {...input} type={type} className='form-control' />
+    {valid && touched && <span className="glyphicon glyphicon-ok form-control-feedback" />}
+    {error && touched && <span className="glyphicon glyphicon-remove form-control-feedback" />}
+    {error && touched && <div className="text-danger"><strong>{error}</strong></div>}
+  </div>
   )
 }
 
 RefitInput.propTypes = {
 	size: PropTypes.oneOf(['lg', 'sm']), // specifiy the size of the form-group
 	labelRequired: PropTypes.bool, // display an astreik in required fields
-	input: PropTypes.object, // from redux-form
+	label: PropTypes.string.isRequired, // passed into Field
+	type: PropTypes.string.isRequired, // passed into Field
+	input: PropTypes.shape({
+		name: PropTypes.string.isRequired,
+	}), // from redux-form and passed in props
+	meta: PropTypes.object, // from redux-form
 }
 
 export default RefitInput;
