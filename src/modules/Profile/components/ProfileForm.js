6b2d2f07c4113064d1244ede73463886039f2499
ericgold <egold05@gmail.com>
2017-11-18 10:05:19 -0800

    fix(Profile): position center Done button

--- src/modules/Profile/components/ProfileForm.js
+++ src/modules/Profile/components/ProfileForm.js
@@ -114,7 +114,7 @@ export default class ProfileForm extends Component {
         <button className={styles.password} type="button">
           Change Password
         </button>
-        <button disabled={pristine || submitting} className="btn btn-default" type="submit">
+        <button disabled={pristine || submitting} className={styles.done} type="submit">
           Done
         </button>
       </form>
