960823f1476184e83d9cb0c299ee99614e75e9ab
ericgold <egold05@gmail.com>
2017-12-07 15:56:15 -0800

    fix(Talk): update bootstrap class name to center text

--- src/modules/Talk/components/Talk.js
+++ src/modules/Talk/components/Talk.js
@@ -12,7 +12,7 @@ const Talk = (props) => {
     <div className={styles.talk}>
       <div className='container'>
         <div className='row'>
-          <div className='col-xs-12 text-center'>
+          <div className='col-12 text-center'>
             {props.pipeline === 'looking' ? <Looking /> : null}
             {props.pipeline === 'listenerFound' ? <ListenerFound /> : null}
             {props.pipeline === 'roomReady' ? <RoomReady /> : null}
