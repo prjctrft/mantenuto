8aac4e4cde926af01256db9447a21c56f983e3ae
Eric Gold <egold05@gmail.com>
2017-12-12 08:33:18 -0800

    refactor(ChatIconButton): use png images to indicate new messages or not (#115)

--- src/modules/Rooms/components/ChatIconButton.js
+++ src/modules/Rooms/components/ChatIconButton.js
@@ -2,13 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import chatIcon from '../assets/ic_private_message.png';
+import chatIconNewMessage from '../assets/ic_private_message_new.png';
+
 const ChatIconButton = (props) => {
   const styles = require('./ChatIconButton.scss');
-
   return (
     <div className={styles.ChatIconButton}>
       <button onClick={props.toggleChat}>
-        <i className="fa fa-comments-o fa-3x" aria-hidden="true" />
+        {props.unreadMessages > 0 ? 
+          <img src={chatIconNewMessage} alt='new chats' />
+          : 
+          <img src={chatIcon} alt='chat icon' /> 
+        }
       </button>
       {props.unreadMessages > 0 ? <p>You have {props.unreadMessages} unread messages!</p> : null }
     </div>
@@ -16,7 +22,8 @@ const ChatIconButton = (props) => {
 }
 
 ChatIconButton.propTypes = {
-  toggleChat: PropTypes.func.isRequired
+  toggleChat: PropTypes.func.isRequired,
+  unreadMessages: PropTypes.array.isRequired
 }
 
 export default connect((state) => ({unreadMessages: state.messages.unreadMessages}))(ChatIconButton);
