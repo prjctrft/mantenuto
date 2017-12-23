5675c37a7740256c574a4e0e831ee0744684f847
ericgold <egold05@gmail.com>
2017-12-22 16:00:56 -0800

    refactor(Listen): Use RefitButton for Preferences component

--- src/modules/Listen/components/Preferences.js
+++ src/modules/Listen/components/Preferences.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types'; 
 
-import CircleButton from 'components/CircleButton';
+import RefitButton from 'components/RefitButton';
 
 import Wrapper from './Wrapper';
 
@@ -11,16 +11,16 @@ const Preferences  = (props) => {
     <Wrapper>
       <div className={styles.btns}>
         <div className='row'>
-          <div className={`col-sm-5 ${styles.noWrap}`}>
+          <div className={`col-sm-5 text-center ${styles.noWrap}`}>
             <h3>Can you listen anytime?</h3>
-            <CircleButton className={`${styles.marginAuto} ${styles.anytime}`} onClick={props.handleAnytime} content='Anytime' />
+            <RefitButton className={`${styles.marginAuto} ${styles.anytime}`} onClick={props.handleAnytime} content='Anytime' />
           </div>
           <div className='col-sm-2 text-center'>
             <h3>Or</h3>
           </div>
-          <div className={`col-sm-5 ${styles.noWrap}`}>
+          <div className={`col-sm-5 text-center ${styles.noWrap}`}>
             <h3>Can you only listen now?</h3>
-            <CircleButton className={`${styles.marginAuto} ${styles.now}`} onClick={props.handleNow} content='Now' />
+            <RefitButton className={`${styles.marginAuto} ${styles.now}`} onClick={props.handleNow} content='Now' />
           </div>
         </div>
       </div>
